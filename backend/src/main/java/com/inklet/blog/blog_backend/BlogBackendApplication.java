aa6e222c5333b5d591aef7b152647f0a40528d7b
frknxdmr <latifurkandemir@gmail.com>
2024-12-10 16:36:49 +0300

    Add backend directory

---
+++ backend/src/main/java/com/inklet/blog/blog_backend/BlogBackendApplication.java
@@ -0,0 +1,22 @@
+package com.inklet.blog.blog_backend;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BlogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogBackendApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner helloWorldRunner() {
+		return args -> {
+			System.out.println("Hello, World!");
+		};
+	}
+
+}
