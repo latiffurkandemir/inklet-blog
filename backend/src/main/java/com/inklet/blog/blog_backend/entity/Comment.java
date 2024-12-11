6e9142168983fc25a600d9ad6cd57262b706496b
frknxdmr <latifurkandemir@gmail.com>
2024-12-11 17:23:34 +0300

    Add entity classes

---
+++ backend/src/main/java/com/inklet/blog/blog_backend/entity/Comment.java
@@ -0,0 +1,86 @@
+package com.inklet.blog.blog_backend.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "content")
+    private String content;
+
+    @ManyToOne(
+            cascade = {CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH, CascadeType.DETACH}
+    )
+    @JoinColumn(name = "post_id")
+    private Blog blog;
+
+    @ManyToOne(
+            cascade = {CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH, CascadeType.DETACH}
+    )
+    @JoinColumn(name = "author_id")
+    private User user;
+
+    public Comment() {
+
+    }
+
+    //for creating a comment
+    public Comment(String content, Blog blog, User user) {
+        this.content = content;
+        this.blog = blog;
+        this.user = user;
+    }
+
+    //for updating a comment
+    public Comment(int id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Blog getBlog() {
+        return blog;
+    }
+
+    public void setBlog(Blog blog) {
+        this.blog = blog;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                ", blog=" + blog +
+                ", user=" + user +
+                '}';
+    }
+}
