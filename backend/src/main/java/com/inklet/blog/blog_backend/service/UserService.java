e293a442f221ec442287c3f5fd28f4056564fd55
frknxdmr <latifurkandemir@gmail.com>
2024-12-13 22:40:22 +0300

    Added create blog endpoint

--- backend/src/main/java/com/inklet/blog/blog_backend/service/UserService.java
+++ backend/src/main/java/com/inklet/blog/blog_backend/service/UserService.java
@@ -1,43 +1,7 @@
 package com.inklet.blog.blog_backend.service;
 
-import com.inklet.blog.blog_backend.dto.LoginDTO;
 import com.inklet.blog.blog_backend.dto.UserDTO;
-import com.inklet.blog.blog_backend.entity.User;
-import com.inklet.blog.blog_backend.exception.InputAlreadyExistsException;
-import com.inklet.blog.blog_backend.exception.InputNotFoundException;
-import com.inklet.blog.blog_backend.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
-@Service
-public class UserService {
-
-    private final UserRepository userRepository;
-    private BCryptPasswordEncoder passwordEncoder;
-
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    public void createUser(UserDTO userDTO) {
-        if (userRepository.findByUsername(userDTO.getUsername()).isPresent()) {
-            throw new InputAlreadyExistsException("Username '" + userDTO.getUsername() + "' already exists.");
-        }
-        if (userRepository.findByEmail(userDTO.getEmail()).isPresent()) {
-            throw new InputAlreadyExistsException("Email '" + userDTO.getEmail() + "' already exists");
-        }
-        User user = new User(
-                userDTO.getUsername(),
-                userDTO.getNickname(),
-                userDTO.getEmail(),
-                passwordEncoder.encode(userDTO.getPassword())
-        );
-        userRepository.save(user);
-    }
-
-
+public interface UserService {
+    void signup(UserDTO userDTO);
 }
