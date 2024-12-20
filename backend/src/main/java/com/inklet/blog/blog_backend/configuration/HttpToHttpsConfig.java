5fd6cf8d270e8415129d783e616078cc833db311
latiffurkandemir <latiffurkan.demir@gmail.com>
2024-12-20 20:58:33 +0300

    Add local ssl enabled

---
+++ backend/src/main/java/com/inklet/blog/blog_backend/configuration/HttpToHttpsConfig.java
@@ -0,0 +1,25 @@
+package com.inklet.blog.blog_backend.configuration;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpToHttpsConfig {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainerCustomizer() {
+        return factory -> factory.addAdditionalTomcatConnectors(createHttpConnector());
+    }
+
+    private org.apache.catalina.connector.Connector createHttpConnector() {
+        org.apache.catalina.connector.Connector connector =
+                new org.apache.catalina.connector.Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
+}
